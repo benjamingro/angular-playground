5eb73ca8bd5d5a971dc228525fe4cd6ce0df1bbd
Ben Gross <benjamingro@gmail.com>
2021-07-28 14:42:43 -0500

    got form working

--- src/app/basic-form/basic-form.component.ts
+++ src/app/basic-form/basic-form.component.ts
@@ -7,14 +7,16 @@ import { FormGroup, FormControl, Validators  } from '@angular/forms';
   styleUrls: ['./basic-form.component.scss']
 })
 export class BasicFormComponent implements OnInit {
+  public submitted = false; 
+
   basicForm = new FormGroup({
-    firstName: new FormControl(''),
-    lastName: new FormControl(''),
-    userName: new FormControl(''),
-    city: new FormControl(''),
-    state: new FormControl(''),
-    zipCode: new FormControl(''),
-    agree: new FormControl(''),
+    firstName: new FormControl('', {validators: Validators.required}),
+    lastName: new FormControl('', {validators: Validators.required}),
+    userName: new FormControl('', {validators: Validators.required}),
+    city: new FormControl('', {validators: Validators.required}),
+    state: new FormControl('', {validators: Validators.required}),
+    zipCode: new FormControl('', {validators: Validators.required}),
+    agree: new FormControl('', {validators: Validators.requiredTrue}),
   });
   constructor() { }
 
@@ -23,6 +25,17 @@ export class BasicFormComponent implements OnInit {
 
   onSubmit() : void {
     console.warn(this.basicForm.value);
+    this.submitted=true; 
   }
+
+  get firstName() { return this.basicForm.get('firstName'); }
+  get lastName() { return this.basicForm.get('lastName'); }
+  get userName() { return this.basicForm.get('userName'); }
+  get city() { return this.basicForm.get('city'); }
+  get state() { return this.basicForm.get('state'); }
+  get zipCode() { return this.basicForm.get('zipCode'); }
+  get agree() { return this.basicForm.get('agree'); }
+
+
 
 }
