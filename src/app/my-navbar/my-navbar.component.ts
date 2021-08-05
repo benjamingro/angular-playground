37adab8ce4eaa004ac525a2cb10e1164334e29a2
Ben Gross <benjamingro@gmail.com>
2021-08-05 10:42:18 -0500

    console.log added to get current_route

--- src/app/my-navbar/my-navbar.component.ts
+++ src/app/my-navbar/my-navbar.component.ts
@@ -12,6 +12,7 @@ export class MyNavbarComponent implements OnInit {
   constructor(private location : Location) { 
     location.onUrlChange((url: string, state: unknown):void=>{
       this.current_route = url; 
+      console.log(`this.current_route = ${this.current_route}`); 
     }); 
   }
 
